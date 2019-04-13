3fa90f5312da8a6da7073f2b9cbc744c481f284c
David Maravilla <bl4ckf4ll@gmail.com>
2019-04-13 12:16:10 -0600

    Add actions and mutations to store site module

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -14,6 +14,7 @@
                 <v-text-field
                   solo-inverted
                   :value="title"
+                  @keyup="updateTitleHandler"
                   label="Site Name"
                 ></v-text-field>
               </v-flex>
@@ -21,6 +22,7 @@
                 <v-text-field
                   solo-inverted
                   :value="description"
+                  @keyup="updateDescriptionHandler"
                   label="Site Description"
                 ></v-text-field>
               </v-flex>
@@ -36,7 +38,7 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex';
+  import { mapState, mapActions } from 'vuex';
   import Posts from './components/Posts';
 
   export default {
@@ -49,6 +51,18 @@
         'title',
         'description',
       ]),
+    },
+    methods: {
+      ...mapActions('site', [
+        'updateTitle',
+        'updateDescription',
+      ]),
+      updateTitleHandler(event) {
+        this.updateTitle(event.target.value);
+      },
+      updateDescriptionHandler(event) {
+        this.updateDescription(event.target.value);
+      },
     },
   };
 </script>
---
+++ resources/js/store/modules/site/actions.js
@@ -0,0 +1,8 @@
+export default {
+  updateTitle({ commit }, title) {
+    commit('updateTitle', title);
+  },
+  updateDescription({ commit }, description) {
+    commit('updateDescription', description);
+  },
+};
