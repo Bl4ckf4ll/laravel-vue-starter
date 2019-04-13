1a913252a18764941465b1493b604c18d3eaabab
David Maravilla <bl4ckf4ll@gmail.com>
2019-04-13 01:29:40 -0600

    Add posts component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -5,12 +5,15 @@
       <p>
         A Laravel starter with <strong>Vue</strong> and <strong>Vuetify</strong>
       </p>
+      <Posts/>
     </v-container>
   </v-app>
 </template>
 
 <script>
+  import Posts from './components/Posts';
   export default {
     name: 'App',
+    components: { Posts },
   };
 </script>
---
+++ resources/js/components/Posts.vue
@@ -0,0 +1,22 @@
+<template>
+</template>
+
+<script>
+  import { index } from '../requests/Posts';
+
+  export default {
+    name: 'Posts',
+    data: () => ({
+      posts: [],
+    }),
+    async created() {
+      const { data } = await index();
+
+      this.posts = data;
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
