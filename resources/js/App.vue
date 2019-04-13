d542fd382727bc70a2432ce9e26001316cbc0a49
David Maravilla <bl4ckf4ll@gmail.com>
2019-04-13 11:32:44 -0600

    Add vuex to app

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app light>
     <v-container>
-      <h1>Vue App</h1>
+      <h1>{{ site.title }}</h1>
       <p>
         A Laravel starter with <strong>Vue</strong> and <strong>Vuetify</strong>
       </p>
@@ -11,9 +11,18 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex';
   import Posts from './components/Posts';
+
   export default {
     name: 'App',
-    components: { Posts },
+    components: {
+      Posts,
+    },
+    computed: {
+      ...mapState([
+        'site',
+      ]),
+    },
   };
 </script>
