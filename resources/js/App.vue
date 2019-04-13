a32c59bd88c310e03229b2273a9894b4d5cacf4b
David Maravilla <bl4ckf4ll@gmail.com>
2019-04-12 23:26:30 -0600

    Downgrade vuetify version

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app light>
-    <h1>Vue App</h1>
+    <v-container text-xs-center>
+      <h1>Vue App</h1>
+      <v-text-field label="name"></v-text-field>
+    </v-container>
   </v-app>
 </template>
 
