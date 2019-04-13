d1d289803ad7305441b5bc91d69ffac86292c850
David Maravilla <bl4ckf4ll@gmail.com>
2019-04-13 11:54:47 -0600

    Add toolbar

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,36 @@
 <template>
   <v-app light>
     <v-container>
-      <h1>{{ title }}</h1>
-      <p v-html="description"></p>
-      <Posts/>
+      <v-toolbar app>
+        <v-toolbar-title>
+          {{ title }}
+          <small class="blue--text" v-html="description"></small>
+        </v-toolbar-title>
+        <v-spacer/>
+        <v-form>
+          <v-container>
+            <v-layout>
+              <v-flex xs6>
+                <v-text-field
+                  solo-inverted
+                  :value="title"
+                  label="Site Name"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs6>
+                <v-text-field
+                  solo-inverted
+                  :value="description"
+                  label="Site Description"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-form>
+      </v-toolbar>
+      <v-content>
+        <Posts/>
+      </v-content>
     </v-container>
   </v-app>
 </template>
