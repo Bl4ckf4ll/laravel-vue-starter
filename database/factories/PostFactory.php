ca04989754d9bd026ce5123bd72927b0de993c19
David Maravilla <bl4ckf4ll@gmail.com>
2019-04-13 00:53:58 -0600

    Add post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\Post;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+
+/** @var Factory $factory */
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->words(),
+        'description' => $faker->paragraph(),
+        'image_url' => $faker->imageUrl(),
+    ];
+});
