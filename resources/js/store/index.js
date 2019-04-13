6327a22a8ea67ae50144659627924d24bcec39a4
David Maravilla <bl4ckf4ll@gmail.com>
2019-04-13 12:18:48 -0600

    Add vuex persisted state

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate'
 import site from './modules/site/main';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [
+    createPersistedState(),
+  ],
   modules: {
     site,
   },
